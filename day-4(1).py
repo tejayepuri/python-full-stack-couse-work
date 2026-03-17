5f67d5896c73a63aef5cd5bcfb38fb98e209308b
Yepuri Teja Naga Anil Kumar <126933993+tejayepuri@users.noreply.github.com>
2026-03-17 17:49:41 +0530

    Add files via upload

---
+++ day-4(1).py
@@ -0,0 +1,101 @@
+Python 3.11.2 (tags/v3.11.2:878ead1, Feb  7 2023, 16:38:35) [MSC v.1934 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+str1="hello"
+str2="world"
+str3='''this is multiline
+string example'''
+print(str1)
+hello
+print(str2)
+world
+print(str3)
+this is multiline
+string example
+str1="hello"
+str2="world"
+result=str1+str2
+print(result)
+helloworld
+print("python!" *5)
+python!python!python!python!python!
+print("teja!"*6)
+teja!teja!teja!teja!teja!teja!
+text="python"
+print(text[0])
+p
+print(text[4])
+o
+print(0:3)
+SyntaxError: invalid syntax
+print(text[0:3])
+pyt
+print(text[2:])
+thon
+print(text[:4])
+pyth
+print('pyt' in text)
+True
+print('teja' not in text)
+True
+print('prasana' in text)
+False
+text="hello world"
+print(len(text))
+11
+print(max(text))
+w
+print(min(text))
+ 
+print(min(text))
+ 
+print(min("sdnkjcnsk))
+          
+SyntaxError: incomplete input
+print(min("kdsdhcxnknzkx"))
+          
+c
+>>> print(max("kdhckxnkk"))
+...           
+x
+>>> print(sorted("python"))
+...           
+['h', 'n', 'o', 'p', 't', 'y']
+>>> print(chr(97))
+...           
+a
+>>> 
+>>> 'hello.upper"
+...           
+SyntaxError: incomplete input
+>>> "hello".upper()
+...           
+'HELLO'
+>>> 'world'.lower()
+...           
+'world'
+>>> 'python'.capatalize()
+...           
+Traceback (most recent call last):
+  File "<pyshell#39>", line 1, in <module>
+    'python'.capatalize()
+AttributeError: 'str' object has no attribute 'capatalize'. Did you mean: 'capitalize'?
+>>> 'python'.capitalize
+...           
+<built-in method capitalize of str object at 0x000001F24235C9B0>
+>>> 'python'.title()
+...           
+'Python'
+>>> 'python'.swapcase()
+...           
+'PYTHON'
+>>> 'hello world'.casefold()
+...           
+'hello world'
+>>> print("hello world".title())
+...           
+Hello World
+>>> text="python"
+...           
+>>> print(10,'*')
+...           
+10 *
