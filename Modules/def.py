f4b98f04d85060e90f7916f5c2ce846fe6bdbffc
Yepuri Teja Naga Anil Kumar <126933993+tejayepuri@users.noreply.github.com>
2026-03-31 19:22:34 +0530

    Add files via upload

---
+++ Modules/def.py
@@ -0,0 +1,66 @@
+'''import logic as lg
+
+print(lg.likes)
+print(lg.comments)
+
+print(lg.addlike())
+print(lg.addlike())
+print(lg.addlike()) 
+print(lg.addcomments("good"))'''
+
+
+'''from logic import likes,comments,addlike,addcomments
+print(likes)
+print(comments)
+
+print(addlike())
+print(addlike())
+print(addlike()) 
+print(addcomments("good"))'''
+
+
+'''from logic import *
+print(likes)
+print(comments)
+
+print(addlike())
+print(addlike())
+print(addlike()) 
+print(addcomments("good"))'''
+
+
+'''import logic
+
+print(logic.add(a,b))
+print(logic.sub(a,b))
+print(logic.mul(a,b))
+print(logic.div(a,b))
+print(logic.mod(a,b))'''
+
+
+'''import logic as lg
+
+print(lg.add(6,4))
+print(lg.sub(6,4))
+print(lg.mul(6,4))
+print(lg.div(6,4))
+print(lg.mod(6,4))'''
+
+'''from logic import *
+
+print(add(6,4))
+print(sub(6,4))
+print(mul(6,4))
+print(div(6,4))
+print(mod(6,4))'''
+
+'''from logic import add,sub,mul,div,mod
+
+print(add(6,4))
+print(sub(6,4))
+print(mul(6,4))
+print(div(6,4))
+print(mod(6,4))'''
+
+
+
---
+++ Modules/logic.py
@@ -0,0 +1,41 @@
+'''likes=0
+
+comments=[]
+
+def addlike():
+    global likes
+    likes+=1
+    return likes
+
+def addcomments(com):
+    global comments
+    comments.append(com)
+    return comments
+
+print(addlike())    
+print(addcomments("good"))'''
+
+'''a=10
+b=5
+
+def add(a,b):
+    return a+b
+
+def sub(a,b):
+    return a-b
+def mul(a,b):
+    return a*b
+def div(a,b):
+    return a/b
+def mod(a,b):
+    return a%b
+print(add(a,b))
+print(sub(a,b))
+print(mul(a,b))
+print(div(a,b))
+print(mod(a,b))'''
+
+
+
+
+
