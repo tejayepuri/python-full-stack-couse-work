9181daaf78cfd998fc1cce06921ced2fe7bcadd8
Yepuri Teja Naga Anil Kumar <126933993+tejayepuri@users.noreply.github.com>
2026-04-16 22:43:18 +0530

    Add files via upload

---
+++ Day-16/inheritance.py
@@ -0,0 +1,12 @@
+class Instagram:
+    def reel(self):
+        print("you can pass the reel")
+teja= Instagram()
+teja.reel()
+
+
+
+class Instagram:
+    def reel(self):
+        print("you can pass the reel")
+print('Anju_InstagramV1')        
